602fe71af6075a0e4ec4d66b7dd93111704e644a
Kevin McCartney <hello@kevinmccartney.is>
2021-02-12 21:59:35 -0500

    Adding tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,52 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders the name title', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByText(/Hello, I am Kevin McCartney/i);
+    expect(nameTitleElement).toBeInTheDocument();
+  });
+
+  test('renders the role title', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByText(/Engineer, mentor, & teacher/i);
+    expect(nameTitleElement).toBeInTheDocument();
+  });
+
+  test('renders linkedin link', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByTestId(
+      'linkedin-link',
+    ) as HTMLLinkElement;
+    expect(nameTitleElement.href).toBe(
+      'https://www.linkedin.com/in/kevinmccartney2',
+    );
+  });
+
+  test('renders twitter link', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByTestId(
+      'twitter-link',
+    ) as HTMLLinkElement;
+    expect(nameTitleElement.href).toBe('https://twitter.com/kmccartneydev');
+  });
+
+  test('renders github link', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByTestId(
+      'github-link',
+    ) as HTMLLinkElement;
+    expect(nameTitleElement.href).toBe('https://github.com/kevinmccartney');
+  });
+
+  test('renders how i was made link', () => {
+    render(<App />);
+    const nameTitleElement = screen.getByTestId(
+      'how-i-was-made-link',
+    ) as HTMLLinkElement;
+    expect(nameTitleElement.href).toBe(
+      'https://github.com/kevinmccartney/kevinmccartney.is',
+    );
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,12 @@ function App() {
         <h1 className="text-8xl pb-24">Hello, I am {name}</h1>
         <h2 className="text-6xl pb-32">{renderRoles(roles)}</h2>
         <div className="flex text-green-300 text-6xl underline">
-          <a href={socialLinks.linkedIn} rel="noreferrer" className="mr-24">
+          <a
+            href={socialLinks.linkedIn}
+            rel="noreferrer"
+            className="mr-24"
+            data-testid="linkedin-link"
+          >
             <i className="fab fa-linkedin"></i>
           </a>
           <a
@@ -39,16 +44,27 @@ function App() {
             target="_blank"
             rel="noreferrer"
             className="mr-24"
+            data-testid="twitter-link"
           >
             <i className="fab fa-twitter"></i>
           </a>
-          <a href={socialLinks.github} target="_blank" rel="noreferrer">
+          <a
+            href={socialLinks.github}
+            target="_blank"
+            rel="noreferrer"
+            data-testid="github-link"
+          >
             <i className="fab fa-github"></i>
           </a>
         </div>
       </div>
       <div className="text-white p-16 flex justify-center text-green-300 text-2xl underline font-extrabold">
-        <a target="_blank" href={howIWasMadeLink} rel="noreferrer">
+        <a
+          target="_blank"
+          href={howIWasMadeLink}
+          rel="noreferrer"
+          data-testid="how-i-was-made-link"
+        >
           How I was made
         </a>
       </div>
