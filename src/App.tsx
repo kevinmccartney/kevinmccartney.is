900fd0e5d9a1a3e271495ef1fe759d6c6a36854b
Kevin McCartney <hello@kevinmccartney.is>
2021-02-12 21:23:20 -0500

    Adding greeting and social links

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const socialLinks = {
+    linkedIn: 'https://www.linkedin.com/in/kevinmccartney2',
+    twitter: 'https://twitter.com/kmccartneydev',
+    github: 'https://github.com/kevinmccartney',
+  };
+  const howIWasMadeLink = 'https://github.com/kevinmccartney/kevinmccartney.is';
+  const name = 'Kevin McCartney';
+  const roles = ['engineer', 'mentor', 'teacher'];
+
+  const renderRoles = (rolesToRender: string[]) =>
+    rolesToRender.reduce((prev: string, current: string, index: number) => {
+      let role: string;
+
+      if (index === 0) {
+        role = `${current.charAt(0).toUpperCase()}${current.slice(1)}, `;
+      } else if (index == roles.length - 1) {
+        role = `& ${current}`;
+      } else {
+        role = `${current}, `;
+      }
+
+      return `${prev}${role}`;
+    }, '');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main className="App flex-col flex flex-column h-screen w-screen bg-black font-mono">
+      <div className="text-white flex-grow-1 flex flex-col justify-center items-center text-center">
+        <h1 className="text-8xl pb-24">Hello, I am {name}</h1>
+        <h2 className="text-6xl pb-32">{renderRoles(roles)}</h2>
+        <div className="flex text-green-300 text-6xl underline">
+          <a href={socialLinks.linkedIn} rel="noreferrer" className="mr-24">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href={socialLinks.twitter}
+            target="_blank"
+            rel="noreferrer"
+            className="mr-24"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href={socialLinks.github} target="_blank" rel="noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+      <div className="text-white p-16 flex justify-center text-green-300 text-2xl underline font-extrabold">
+        <a target="_blank" href={howIWasMadeLink} rel="noreferrer">
+          How I was made
         </a>
-      </header>
-    </div>
+      </div>
+    </main>
   );
 }
 
