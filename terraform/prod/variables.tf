4dda8a0f793874da4b1f070add4619bd33f3f866
Kevin McCartney <hello@kevinmccartney.is>
2021-02-13 17:27:24 -0500

    Adding cloudfront distributions and route53 records

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,9 @@
+variable "portfolio_aws_region" {
+  type        = string
+  description = "The AWS region in which our serverless resources reside"
+}
+
+variable "portfolio_env" {
+  type        = string
+  description = "The environment we are working on"
+}
